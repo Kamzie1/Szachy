02ceab8e25f96fb2f39e5f4109f0e639751755fe
kamzi <kamilzie07@gmail.com>
2025-05-22 17:34:09 +0200

    Chat and Stats instance

--- project/website/models.py
+++ project/website/models.py
@@ -1,9 +1,76 @@
 from . import db
 from flask_login import UserMixin
+from datetime import datetime
+from enum import Enum
+import sqlalchemy
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(200))
+    date = db.Column(db.DateTime(), default = datetime.utcnow)
 
     elo = db.Column(db.Integer, default=1000)
+
+    games_as_white = db.relationship('Game', back_populates='white_player',
+                                     foreign_keys='Game.white_id', lazy='dynamic')
+    games_as_black = db.relationship('Game', back_populates='black_player',
+                                     foreign_keys='Game.black_id', lazy='dynamic')
+
+    chat_user1 = db.relationship('Chat', back_populates='user1',
+                                     foreign_keys='Chat.user1_id', lazy='dynamic')
+    chat_user2 = db.relationship('Chat', back_populates='user2',
+                                     foreign_keys='Chat.user2_id', lazy='dynamic')
+    
+    chat_user = db.relationship('Chat_log', back_populates='user',
+                                     foreign_keys='Chat_log.nadawca', lazy='dynamic')
+
+class ResultEnum(Enum):
+    WHITE_WIN = 1
+    DRAW = 0
+    BLACK_WIN = -1
+
+class Game(db.Model):
+    __tablename__ = 'games'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    result = db.Column(sqlalchemy.Enum(ResultEnum))
+    date = db.Column(db.DateTime(), default = datetime.utcnow)
+
+    white_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    black_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    white_elo = db.Column(db.Integer)
+    black_elo = db.Column(db.Integer)
+
+    white_player = db.relationship('User', foreign_keys=[white_id], back_populates='games_as_white')
+    black_player = db.relationship('User', foreign_keys=[black_id], back_populates='games_as_black')
+
+
+class Chat(db.Model):
+    __tablename__ = 'chat'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    user1 = db.relationship('User', foreign_keys=[user1_id], back_populates='chat_user1')
+    user2 = db.relationship('User', foreign_keys=[user2_id], back_populates='chat_user2')
+
+    chat_log = db.relationship('Chat_log', back_populates='chat',
+                                     foreign_keys='Chat_log.chat_id', lazy='dynamic')
+
+
+class Chat_log(db.Model):
+    __tablename__ = 'chat_logs'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    chat_id = db.Column(db.Integer(), db.ForeignKey('chat.id'), nullable = False)
+    nadawca = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable = False)
+    date = db.Column(db.DateTime(), default = datetime.utcnow)
+    message = db.Column(db.String(1000))
+
+    chat = db.relationship('Chat', foreign_keys=[chat_id], back_populates='chat_log')
+    user = db.relationship('User', foreign_keys=[nadawca], back_populates='chat_user')
--- project/website/view/view.py
+++ project/website/view/view.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, flash
 from flask_login import current_user, login_required
-from ..models import User
+from ..models import User, Game, Chat, Chat_log
+from .. import db
 
 view = Blueprint(
     "view",
@@ -13,7 +14,63 @@ view = Blueprint(
 
 
 @view.route('/')
-@login_required
 def home():
-    users = User.query.all()
-    return render_template("home.html", users = users)
+    if current_user.is_authenticated:
+        return redirect(url_for('view.stats', user_id = current_user.id))
+    else:
+        return redirect(url_for('auth.login'))
+
+def find_chat_id(user_id):
+    chat = Chat.query.filter(
+        (
+        (Chat.user1_id == current_user.id) &
+        (Chat.user2_id == user_id)
+        ) |
+        (
+        (Chat.user2_id == current_user.id) &
+        (Chat.user1_id == user_id)
+        )
+    ).first()
+
+    if chat is None:
+        new_chat = Chat(user1_id = current_user.id, user2_id = user_id)
+        db.session.add(new_chat)
+        db.session.commit()
+        chat = new_chat
+
+    return chat.id
+
+
+@view.route('/search')
+@login_required
+def search():
+    query = request.args.get('q') + "%"
+    
+    if query:
+        shows = User.query.filter((User.name != current_user.name) & (User.name.like(query))).all()
+    else:
+        shows = []
+    user_list = [{"id": u.id, "name": u.name, "elo": u.elo, "chat": find_chat_id(u.id)} for u in shows]
+    return jsonify(user_list)
+
+@view.route('/stats<user_id>')
+@login_required
+def stats(user_id):
+    games = Game.query.filter(
+        (user_id == Game.white_id) or
+        (user_id == Game.black_id)
+    ).order_by(Game.date.desc()).all()
+
+    return render_template('stats.html', games=games, c_user = current_user)
+
+@view.route('/chat<chat_id>')
+@login_required
+def chat(chat_id):
+    chat = Chat.query.filter(chat_id == Chat.id).first()
+    if chat:
+        messages = Chat_log.query.filter(chat.id == Chat_log.chat_id).all()
+    else:
+        flash("Coś poszło nie tak", category='error')
+        return redirect(url_for('view.home'))
+
+    return render_template('chat.html', c_user = current_user, chat = chat, messages = messages)
